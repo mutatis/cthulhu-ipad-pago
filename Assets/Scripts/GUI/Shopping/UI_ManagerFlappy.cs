e744f780fb8e3b0cf41d2f5510d3bd99b019daa4
Gabriel <mutatisguga@gmail.com>
2015-08-31 13:58:27 -0300

    f

---
+++ Assets/Scripts/GUI/Shopping/UI_ManagerFlappy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_ManagerFlappy : MonoBehaviour {
+
+	public int value;
+	public GameObject valor;
+	public GameObject cash;
+	
+	void Start ()
+	{
+		if (PlayerPrefs.GetInt("hasFlappy") == 1)
+		{
+			valor.SetActive(false);
+			cash.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/UI_ManagerIndi.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_ManagerIndi : MonoBehaviour {
+
+	public int value;
+	public GameObject valor;
+	public GameObject cash;
+	
+	void Start ()
+	{
+		if (PlayerPrefs.GetInt("hasIndi") == 1)
+		{
+			valor.SetActive(false);
+			cash.SetActive(false);
+		}
+	}
+}
